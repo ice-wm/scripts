21462bae9d6da38e4910ef008bf512245a86f41b
Bert Gijsbers <gijsbers@science.uva.nl>
2025-03-11 12:49:13 +0100

    On debian use libgdk-pixbuf-2.0-dev.

--- os-depends.sh
+++ os-depends.sh
@@ -69,7 +69,7 @@ debian () {
     t=`mktemp`
     apt list --installed >$t || fail "apt list --installed failed"
     sed -i -e '/-base\//d' $t
-    r=" asciidoctor autoconf automake autopoint build-essential cmake fonts-dejavu gettext git libfontconfig1-dev libfribidi-dev libgdk-pixbuf2.0-dev libimlib2-dev libtool libsm-dev libx11-dev libxext-dev libxft-dev libxinerama-dev libxrandr-dev libxrender-dev libxcomposite-dev libxdamage-dev libxfixes-dev libpng-dev libjpeg-dev libxpm-dev librsvg2-dev lzip markdown x11-utils xterm xdg-utils yad "
+    r=" asciidoctor autoconf automake autopoint build-essential cmake fonts-dejavu gettext git libfontconfig1-dev libfribidi-dev libgdk-pixbuf-2.0-dev libimlib2-dev libtool libsm-dev libx11-dev libxext-dev libxft-dev libxinerama-dev libxrandr-dev libxrender-dev libxcomposite-dev libxdamage-dev libxfixes-dev libpng-dev libjpeg-dev libxpm-dev librsvg2-dev lzip markdown x11-utils xterm xdg-utils yad "
     [ $sound = 1 ] && snd=" libao-dev libasound2-dev libsndfile1-dev libpulse-dev" || snd=
     [ $noask = 1 ] && ask=--yes || ask=
     i=
